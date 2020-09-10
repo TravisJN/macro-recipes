2df37b53fe616d4e80cfc6e17e656fee1348cc31
Travis Neufeld <tdizzletrav@gmail.com>
2020-09-10 16:16:39 -0700

    Add mock data

--- data/Store.js
+++ data/Store.js
@@ -3,7 +3,7 @@ import emitter from 'tiny-emitter/instance';
 
 export default class Store {
   static RECIPES_URL = "https://api.spoonacular.com/recipes/findByNutrients?";
-  static RECIPE_URL = `https://api.spoonacular.com/recipes/${id}/information?includeNutrition=true&`;
+  // static RECIPE_URL = `https://api.spoonacular.com/recipes/${id}/information?includeNutrition=true&`;
   static API_KEY = api.key;
 
   static storeInstance = null;
@@ -17,6 +17,7 @@ export default class Store {
 
   mResults = null;
   mIsFetching = false;
+  mIsMockData = false;  // set to true to use mock data
 
   get hasData() {
     return !!this.mResults;
@@ -30,21 +31,28 @@ export default class Store {
     this.mIsFetching = true;
     this.mResults = null;
 
-    try {
-      const params = new URLSearchParams({
-        apiKey: Store.API_KEY,
-        maxProtein: protein,
-        maxFat: fat,
-        maxCarbs: carbs,
-      });
-      console.log('making request:', Store.RECIPES_URL + params);
-      const response = await fetch(Store.RECIPES_URL + params);
-      const result = await response.json();
-      this.mResults = result;
-      emitter.emit('success', result);
-    } catch(e) {
-      console.log('Error fetching recipes', e);
-      emitter.emit('error', {error: e});
+    if (this.mIsMockData) {
+      setTimeout(() => {
+        this.mResults = require('./mockRecipesList.json');
+        emitter.emit('success', this.mResults);
+      }, 10);
+    } else {
+      try {
+        const params = new URLSearchParams({
+          apiKey: Store.API_KEY,
+          maxProtein: protein,
+          maxFat: fat,
+          maxCarbs: carbs,
+        });
+        console.log('making request:', Store.RECIPES_URL + params);
+        const response = await fetch(Store.RECIPES_URL + params);
+        const result = await response.json();
+        this.mResults = result;
+        emitter.emit('success', result);
+      } catch(e) {
+        console.log('Error fetching recipes', e);
+        emitter.emit('error', {error: e});
+      }
     }
 
     this.mIsFetching = false;
@@ -55,17 +63,24 @@ export default class Store {
 
     const url = `https://api.spoonacular.com/recipes/${id}/information?includeNutrition=true&`;
 
-    try {
-      const params = new URLSearchParams({
-        apiKey: Store.API_KEY,
-      });
-      console.log('making request:', url + params);
-      const response = await fetch(url + params);
-      const result = await response.json();
-      emitter.emit('success', result);
-    } catch(e) {
-      console.log(`Error fetching recipe id ${id}`, e);
-      emitter.emit('error', {error: e});
+    if (this.mIsMockData) {
+      setTimeout(() => {
+        const result = require('./mockRecipe.json');
+        emitter.emit('success', result);
+      }, 10);
+    } else {
+      try {
+        const params = new URLSearchParams({
+          apiKey: Store.API_KEY,
+        });
+        console.log('making request:', url + params);
+        const response = await fetch(url + params);
+        const result = await response.json();
+        emitter.emit('success', result);
+      } catch(e) {
+        console.log(`Error fetching recipe id ${id}`, e);
+        emitter.emit('error', {error: e});
+      }
     }
 
     this.mIsFetching = false;
